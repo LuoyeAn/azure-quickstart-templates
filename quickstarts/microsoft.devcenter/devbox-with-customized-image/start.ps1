129b706edf0ab53f76b96b098c57a4086f65f343
Luoye An <antingting2009@qq.com>
2024-03-01 18:00:03 +0800

    commit

--- quickstarts/microsoft.devcenter/devbox-with-customized-image/start.ps1
+++ quickstarts/microsoft.devcenter/devbox-with-customized-image/start.ps1
@@ -8,29 +8,25 @@ if($userPrincipalId){
     az group deployment create -g $resourceGroupName -f main.bicep --parameters userPrincipalId=$userPrincipalId
 
 # 卸载 Microsoft Teams
-  Get-AppxPackage -AllUsers | Where-Object { $_.Name -match MicrosoftTeams } | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.Teams | Remove-AppxPackage -AllUsers
 
   # 卸载 OneDrive
   taskkill /f /im OneDrive.exe
-  Start-Process "$env:SYSTEMROOTSysWOW64OneDriveSetup.exe "/uninstall -NoNewWindow -Wait
-  Start-Process "$env:SYSTEMROOTSystem32OneDriveSetup.exe "/uninstall -NoNewWindow -Wait
+  %SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
+  %SystemRoot%\SysWOW32\OneDriveSetup.exe /uninstall
 
   # 创建 Excel 快捷方式
-  $WScriptShell = New-Object -ComObject WScript.Shell
-  $Shortcut = $WScriptShell.CreateShortcut("$env:PUBLICDesktopExcel.lnk)
-  $Shortcut.TargetPath = C:Program FilesMicrosoft OfficerootOfficeXXEXCEL.EXE # 请根据实际情况替换 XX 为你的 Office 版本号
-  $Shortcut.Save()
+   $WshShell = New-Object -comObject WScript.Shell
+   $Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Desktop\Excel Link.lnk")
+   $Shortcut.TargetPath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+   $Shortcut.Arguments = "https://bopoda-my.sharepoint.com/:x:/r/personal/jzfjteacherlab_geekcopilot_net/_layouts/15/doc2.aspx?sourcedoc={80B38B64-13DF-4CFE-B66C-D46DA8665C71}&file=%25u5b66%25u5458%25u4fe1%25u606f%25u767b%25u8bb0%25u8868.xlsx&action=default&mobileredirect=true&wdLOR=c4E1EDFE8-BADA-4939-8807-C0AC130D12B8"
+   $Shortcut.Save()
 
   # 安装中文输入法
-  $LangList = New-WinUserLanguageList en-US
-  $LangList.Add(zh-CN)
-  Set-WinUserLanguageList $LangList -Force
-
-  # 设置中文输入法为默认
-  $InputMethod = New-WinUserLanguageList zh-CN
-  $InputMethod[0].InputMethodTips.Add(0409:00000409) # 英语 (美国) 键盘
-  $InputMethod[0].InputMethodTips.Add(0804:00000804) # 中文 (简体，中国) - 微软拼音
-  Set-WinUserLanguageList $InputMethod -Force
+    $LangList = New-WinUserLanguageList zh-CN
+   Set-WinUserLanguageList $LangList
+   $LangList[0].InputMethodTips.Add("0409:00000804") # Microsoft Pinyin
+   Set-WinUserLanguageList $LangList -Force
 }else {
     Write-Host "User Principal Name cannot be found."
 }
